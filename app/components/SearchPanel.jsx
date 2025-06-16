2cca676865ec686f74ce786d452288a1959ff16a
joaquincodigo <joaquin.codigo@gmail.com>
2025-06-16 17:44:05 -0300

    Advancing slowly towards complex UI

--- app/components/SearchPanel.jsx
+++ app/components/SearchPanel.jsx
@@ -6,27 +6,37 @@ export default function SearchPanel({ constraintRef }) {
 
   const styles = {
     container: "absolute left-0 w-full",
-    handle: "bg-blue-800 text-white h-8 w-full cursor-row-resize",
+    handle: "bg-blue-800 text-white h-15 w-full cursor-row-resize",
     panel: "bg-blue-200 w-full min-h-72",
   };
 
   return (
     <div className={styles.container}>
+
+
+
+
       {/* DRAGGABLE HANDLE */}
       <motion.div
         drag="y"
         style={{ y }}
         dragConstraints={constraintRef}
         className={styles.handle}
-      />
+      >
+        <p>I am handle</p>
+      </motion.div>
+
+
 
       {/* PANEL THAT FOLLOWS HANDLE */}
-      <motion.div
-        style={{ y }}
-        className={styles.panel}
-      >
+      <motion.div style={{ y }} className={styles.panel}>
         I am the content panel
       </motion.div>
+
+
+
+
+
     </div>
   );
 }
--- app/components/SearchPanelWithConstraints.jsx
+++ app/components/SearchPanelWithConstraints.jsx
@@ -4,14 +4,17 @@ import SearchPanel from "@/app/components/SearchPanel";
 
 export default function SearchPanelWithConstraints() {
   const styles = {
-    constraintArea: "border border-2 border-red-500 rounded-xl h-screen mt-10 mb-10",
+    wrapper: "h-full pt-10 overflow-hidden", // add padding without affecting constraint
+    constraintArea: "relative h-full border border-2 border-red-500 rounded-xl",
   };
 
   const ref = useRef(null);
 
   return (
-    <div ref={ref} className={styles.constraintArea}>
-      <SearchPanel constraintRef={ref} />
+    <div className={styles.wrapper}>
+      <div ref={ref} className={styles.constraintArea}>
+        <SearchPanel constraintRef={ref} />
+      </div>
     </div>
   );
 }
