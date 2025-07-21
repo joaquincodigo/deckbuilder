74c46c93428be184210ef852b257371d065fe6f0
joaquincodigo <joaquin.codigo@gmail.com>
2025-07-21 09:51:02 -0300

    Cleaned comments

--- app/components/EdgeIndicator.jsx
+++ app/components/EdgeIndicator.jsx
@@ -1,11 +1,3 @@
-import { style } from "motion/react-client";
-
-/*
-RIP:
- Having this object as fixed doesnt work because switching causes glitchy behavior.
- You have to add it into the body of the screen and then do a wrapper for what the old body was. This new wrapper should have the paddings.
-*/
-
 export default function EdgeIndicator() {
   const styles = {
     body: "flex flex-row items-center h-screen w-6 bg-blue-400",
