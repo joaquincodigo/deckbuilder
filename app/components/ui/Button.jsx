8d8cdb4b293a3870fc6fb8542b857c1aec64b839
joaquincodigo <joaquin.codigo@gmail.com>
2025-08-11 17:33:47 -0300

    Fixed problem with buttons not triggering: forget onclick, onPointerDown is king!

--- app/components/ui/Button.jsx
+++ app/components/ui/Button.jsx
@@ -5,7 +5,11 @@ import { motion } from "motion/react";
 export default function Button({ children, onClick, className = "" }) {
   return (
     <motion.button
-      onClick={onClick}
+      type="button"
+      onPointerDown={(e) => {
+        e.preventDefault(); // avoid focus/drag delay
+        onClick?.(e);
+      }}
       whileTap={{ scale: 0.9 }}
       className={`${className}`}
     >
