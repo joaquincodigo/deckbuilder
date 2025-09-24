b0ba6431d5855915492cb44cf4955fc8989e58c0
joaquincodigo <joaquin.codigo@gmail.com>
2025-09-24 17:02:57 -0300

    Implementing the new virtualized grid

--- app/components/all-cards-screen/cards-list/CardsList.jsx
+++ app/components/all-cards-screen/cards-list/CardsList.jsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState, useEffect, useRef, useCallback } from "react";
+import { Grid } from "react-window";
+
 import CardCell from "./card-cell/CardCell";
 import CardModal from "../../modals/CardModal/CardModal";
 import LoadingSpinner from "../../ui/LoadingSpinner";
@@ -10,9 +12,12 @@ export default function CardsList() {
   const [isLoading, setIsLoading] = useState(false);
   const [selectedCard, setSelectedCard] = useState(null);
   const [isCardModalOpen, setIsCardModalOpen] = useState(false);
+  const [containerSize, setContainerSize] = useState({ width: 0, height: 0 });
+
   const currentCardsRef = useRef(currentCards);
   const observerRef = useRef(null);
   const gridRef = useRef(null);
+  const containerRef = useRef(null);
 
   useEffect(() => {
     // This is needed to avoid stale state in the function loadMoreCards
@@ -29,6 +34,17 @@ export default function CardsList() {
     setCurrentCards((prev) => [...prev, ...newCards]);
     setIsLoading(false);
   };
+
+  // TESTING-TESTING-TESTING-TESTING-TESTING-TESTING
+  useEffect(() => {
+    fetchMoreCards();
+  }, []);
+
+  useEffect(() => {
+    console.log(containerSize);
+  }, [containerSize]);
+
+  // TESTING-TESTING-TESTING-TESTING-TESTING-TESTING
 
   const sentinelRef = (node) => {
     // This gets executed when the sentinel div mounts
@@ -61,14 +77,23 @@ export default function CardsList() {
     }
   };
 
-  // TESTING-TESTING-TESTING-TESTING-TESTING-TESTING
-  // useEffect(() => {
-  //   console.log("CARDS:", currentCards);
-  // }, [currentCards]);
-  // TESTING-TESTING-TESTING-TESTING-TESTING-TESTING
+  useEffect(() => {
+    if (!containerRef.current) return;
+    const observer = new ResizeObserver(([entry]) => {
+      const { width, height } = entry.contentRect;
+      setContainerSize({ width, height });
+    });
+
+    observer.observe(containerRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
   const styles = {
-    grid: "grid grid-cols-3 flex-grow overflow-y-scroll touch-pan-y bg-amber-800",
+    container: "flex-grow bg-green-400",
+    grid: "grid grid-cols-3 flex-grow overflow-y-scroll touch-pan-y bg-amber-800", // FIX ME
   };
 
   if (!currentCards) {
@@ -76,35 +101,60 @@ export default function CardsList() {
   }
 
   return (
-    <div ref={gridRef} className={styles.grid}>
-      {/* The actual cards list */}
-      {currentCards.map((card) => (
-        <CardCell
-          key={card.id}
-          card={card}
-          isSelected={card.id === selectedCard?.id}
-          onSelect={() => {
-            setSelectedCard(card);
-          }}
-          onShowInfo={() => {
-            setIsCardModalOpen(true);
-          }}
-          onRemoveFromDeck={() => {
-            alert("onRemoveDeck: not implemented yet");
-          }}
-        />
-      ))}
-
-      {/* Sentinel Div for fetching */}
-      <div ref={sentinelRef} className="bg-yellow-400 col-span-3 h-12" />
-
-      {/* Modal for showing card details */}
-      <CardModal
-        isOpen={isCardModalOpen}
-        card={selectedCard}
-        onBackdropClick={() => setIsCardModalOpen(false)}
-        onBodyClick={() => setIsCardModalOpen(false)}
+    <div ref={containerRef} className={styles.container}>
+      <Grid
+        cellComponent={CardCell}
+        cellProps={{
+          currentCards,
+          selectedCard,
+          setSelectedCard,
+          setIsCardModalOpen,
+          onRemoveFromDeck: () => alert("onRemoveDeck: not implemented yet"),
+          onShowInfo: () => {},
+        }}
+        columnCount={3}
+        rowCount={Math.ceil(currentCards.length / 3)}
+        columnWidth={containerSize.width / 3}
+        rowHeight={(containerSize.width / 3) * 1.45762} // 59:86 Ratio
+        overscanCount={10}
+        style={{
+          // Using Tailwind doesnt work
+          width: `${containerSize.width}px`, // total grid width
+          height: `${containerRef.height}px`, // total grid height
+          backgroundColor: "#f0f0f0",
+        }}
       />
     </div>
+
+    // <div ref={gridRef} className={styles.grid}>
+    //   {/* The actual cards list */}
+    //   {currentCards.map((card) => (
+    //     <CardCell
+    //       key={card.id}
+    //       card={card}
+    //       isSelected={card.id === selectedCard?.id}
+    //       onSelect={() => {
+    //         setSelectedCard(card);
+    //       }}
+    //       onShowInfo={() => {
+    //         setIsCardModalOpen(true);
+    //       }}
+    //       onRemoveFromDeck={() => {
+    //         alert("onRemoveDeck: not implemented yet");
+    //       }}
+    //     />
+    //   ))}
+
+    //   {/* Sentinel Div for fetching */}
+    //   <div ref={sentinelRef} className="bg-yellow-400 col-span-3 h-12" />
+
+    //   {/* Modal for showing card details */}
+    //   <CardModal
+    //     isOpen={isCardModalOpen}
+    //     card={selectedCard}
+    //     onBackdropClick={() => setIsCardModalOpen(false)}
+    //     onBodyClick={() => setIsCardModalOpen(false)}
+    //   />
+    // </div>
   );
 }
--- app/components/all-cards-screen/cards-list/card-cell/CardCell.jsx
+++ app/components/all-cards-screen/cards-list/card-cell/CardCell.jsx
@@ -3,13 +3,21 @@ import CardSelector from "./card-selector/CardSelector";
 import { useState } from "react";
 
 export default function CardCell({
-  card,
-  isSelected,
-  onSelect,
+  rowIndex, // from react-window <Grid> component
+  columnIndex, // from react-window <Grid> component
+  style, // from react-window <Grid> component
+  currentCards,
+  selectedCard,
+  setSelectedCard,
+  setIsCardModalOpen,
   onShowInfo,
   onRemoveFromDeck,
 }) {
   const [loading, setLoading] = useState(true);
+
+  const index = rowIndex * 3 + columnIndex;
+  const card = currentCards[index];
+  if (!card) return null;
 
   const styles = {
     cell: "relative overflow-visible aspect-[59/86] bg-gray-900", // gray background as fallback
@@ -17,21 +25,33 @@ export default function CardCell({
   };
 
   return (
-    <div className={styles.cell} onClick={onSelect}>
-      <Image
-        src={`/card_images/${card.id}.jpg`}
-        alt="dummy card image"
-        fill={true}
-        sizes="25vw"
-        className={`${styles.img} ${loading ? "opacity-0" : "opacity-100"}`}
-        onLoad={() => setLoading(false)}
-      />
-      {isSelected && (
-        <CardSelector
-          onShowInfo={onShowInfo}
-          onRemoveFromDeck={onRemoveFromDeck}
-        />
-      )}
+    // <div
+    //   className={styles.cell}
+    //   onClick={() => {
+    //     setSelectedCard(card);
+    //   }}
+    // >
+    //   <Image
+    //     src={`/card_images/${card.id}.jpg`}
+    //     alt="dummy card image"
+    //     // fill={true}
+    //     // sizes="25vw"
+    //     width={59}
+    //     height={86}
+    //     className={`${styles.img} ${loading ? "opacity-0" : "opacity-100"}`}
+    //     onLoad={() => setLoading(false)}
+    //   />
+    //   {selectedCard?.id === card.id && (
+    //     <CardSelector
+    //       onShowInfo={onShowInfo}
+    //       onRemoveFromDeck={onRemoveFromDeck}
+    //     />
+    //   )}
+    // </div>
+
+    <div className="bg-red-500 border-1 text-4xl" style={style}>
+      {index}
     </div>
+    // <></>
   );
 }
