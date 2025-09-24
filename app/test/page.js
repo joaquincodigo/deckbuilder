76152c4ee1860d4a22b3f76b519425d399c36466
joaquincodigo <joaquin.codigo@gmail.com>
2025-09-24 11:43:25 -0300

    Testing react-window

---
+++ app/test/page.js
@@ -0,0 +1,61 @@
+"use client";
+
+import { List } from "react-window";
+import { useEffect, useState } from "react";
+import { names } from "./names.js";
+
+// const groupInThree = (array) => {
+//   if (array.length % 3 === 0) {
+//     for (let i = 0; i < array.length; i + 3) {
+
+//     }
+//   } else {
+//     for (let i = 0; i < array.length; i + 3) {
+//       const element = array[i];
+//       console.log(element);
+//     }
+//   }
+// };
+
+function RowComponent({ index, names, style }) {
+  return (
+    <div
+      className="bg-slate-200 flex items-center justify-between"
+      style={style}
+    >
+      {names[index]}
+    </div>
+  );
+}
+
+export default function Example() {
+  const [list, setList] = useState(names);
+
+  const addItem = () => {
+    const now = new Date();
+    const nowString = now.toLocaleTimeString();
+    setList((prev) => [...prev, `Name ${nowString}`]);
+  };
+
+  useEffect(() => {
+    console.log(list);
+  }, [list]);
+
+  return (
+    <>
+      <button
+        className="bg-blue-500 text-white p-2 rounded-full"
+        onClick={() => addItem()}
+      >
+        click me
+      </button>
+      <List
+        className="h-[600px] overflow-scroll"
+        rowComponent={RowComponent}
+        rowCount={list.length}
+        rowHeight={25}
+        rowProps={{ names: list }}
+      />
+    </>
+  );
+}
---
+++ app/test2/page.js
@@ -0,0 +1,152 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { names } from "./names.js";
+
+import { Grid } from "react-window";
+
+// const names = [
+//   "jhon",
+//   "elisa,",
+//   "james",
+//   "foko",
+//   "rudolf",
+//   "joanna",
+//   "reiina,",
+//   "sarama",
+//   "jhon",
+//   "elisa,",
+//   "james",
+//   "foko",
+//   "rudolf",
+//   "joanna",
+//   "reiina,",
+//   "sarama",
+//   "jhon",
+//   "elisa,",
+//   "james",
+//   "foko",
+//   "rudolf",
+//   "joanna",
+//   "reiina,",
+//   "sarama",
+//   "jhon",
+//   "jhon",
+//   "jhon",
+//   "jhon",
+//   "jhon",
+//   "jhon",
+//   "elisa,",
+//   "james",
+//   "foko",
+//   "rudolf",
+//   "joanna",
+//   "reiina,",
+//   "sarama",
+//   "elisa,",
+//   "james",
+//   "foko",
+//   "rudolf",
+//   "joanna",
+//   "reiina,",
+//   "sarama",
+//   "elisa,",
+//   "james",
+//   "foko",
+//   "rudolf",
+//   "joanna",
+//   "reiina,",
+//   "sarama",
+//   "elisa,",
+//   "james",
+//   "foko",
+//   "rudolf",
+//   "joanna",
+//   "reiina,",
+//   "sarama",
+//   "elisa,",
+//   "james",
+//   "foko",
+//   "rudolf",
+//   "joanna",
+//   "reiina,",
+//   "sarama",
+//   "elisa,",
+//   "james",
+//   "foko",
+//   "rudolf",
+//   "joanna",
+//   "reiina,",
+//   "sarama",
+//   "elisa,",
+//   "james",
+//   "foko",
+//   "rudolf",
+//   "joanna",
+//   "reiina,",
+//   "sarama",
+//   "elisa,",
+//   "james",
+//   "foko",
+//   "rudolf",
+//   "joanna",
+//   "reiina,",
+//   "sarama",
+//   "elisa,",
+//   "james",
+//   "foko",
+//   "rudolf",
+//   "joanna",
+//   "reiina,",
+//   "sarama",
+// ];
+
+function CellComponent({ list, columnIndex, rowIndex, style }) {
+  const itemIndex = rowIndex * 3 + columnIndex;
+
+  return (
+    <div className="truncate bg-amber-200 border-1" style={style}>
+      {itemIndex} {list[itemIndex]}
+    </div>
+  );
+}
+
+export default function Example2() {
+  const [list, setList] = useState(names);
+
+  const addItem = () => {
+    const now = new Date();
+    const nowString = now.toLocaleTimeString();
+    setList((prev) => [...prev, `Name ${nowString}`]);
+  };
+
+  useEffect(() => {
+    console.log(list);
+  }, [list]);
+
+  return (
+    <>
+      <button
+        className="bg-blue-500 text-white p-2 rounded-full"
+        onClick={() => addItem()}
+      >
+        Add item
+      </button>
+
+      <Grid
+        cellComponent={CellComponent}
+        cellProps={{ list }}
+        columnCount={3}
+        columnWidth={100}
+        rowCount={Math.ceil(list.length / 3)}
+        rowHeight={100}
+        style={{
+          width: 320, // total grid width
+          height: 400, // total grid height
+          border: "1px solid black",
+          backgroundColor: "#f0f0f0",
+        }}
+      />
+    </>
+  );
+}
