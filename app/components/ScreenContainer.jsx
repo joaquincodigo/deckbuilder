414b4ae4337e20c2574aa4f8c01384d05d639c4b
joaquincodigo <joaquin.codigo@gmail.com>
2025-10-13 13:13:43 -0300

    Improved shadows

--- app/components/ScreenContainer.jsx
+++ app/components/ScreenContainer.jsx
@@ -1,9 +1,14 @@
 import DeckScreen from "./deck-screen/DeckScreen";
 import AllCardsScreen from "./all-cards-screen/AllCardsScreen";
+import { useScreen } from "../context/ScreenContext";
 
 export default function ScreenContainer() {
+  const { currentScreen } = useScreen();
+
   const styles = {
-    container: "flex-1 relative bg-green-500 drop-shadow-xl/30 z-30",
+    container: `flex-1 relative bg-green-500  z-30 ${
+      currentScreen === "DeckScreen" ? "drop-shadow-xl/30" : "drop-shadow-xl/40"
+    }`,
   };
 
   return (
