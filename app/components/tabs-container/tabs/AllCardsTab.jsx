f58379004f8ce017a876da96d3f94906f2107fd4
joaquincodigo <joaquin.codigo@gmail.com>
2025-10-12 21:01:12 -0300

    Trying to fix sub-pixel rendering problems

--- app/components/tabs-container/tabs/AllCardsTab.jsx
+++ app/components/tabs-container/tabs/AllCardsTab.jsx
@@ -1,13 +1,15 @@
 import { useScreen } from "@/app/context/ScreenContext";
 
 export default function AllCardsNavButton() {
-  const {currentScreen, setCurrentScreen } = useScreen();
+  const { currentScreen, setCurrentScreen } = useScreen();
 
   const styles = {
-    button: `h-full w-1/2 ${
+    button: `relative h-full w-1/2 ${
       currentScreen === "AllCardsScreen" ? "bg-blue-500" : ""
     }`,
-    
+    topCornerMask:
+      "absolute h-3/10 aspect-square top-0 left-0 -translate-x-full",
+    bottomCornerMask: "absolute h-1/4 aspect-square bottom-0 left-0",
   };
 
   return (
@@ -16,6 +18,26 @@ export default function AllCardsNavButton() {
       onMouseDown={() => setCurrentScreen("AllCardsScreen")}
       className={styles.button}
     >
+      {currentScreen === "AllCardsScreen" && (
+        <>
+          <div className={styles.topCornerMask}>
+            <svg className="w-full h-full" viewBox="0 0 100 100">
+              <path
+                d="M0 0 L100 0 L100 100 A100 100 0 0 0 0 0 Z"
+                fill="#2b7fff"
+              />
+            </svg>
+          </div>
+          <div className={styles.bottomCornerMask}>
+            <svg className="w-full h-full" viewBox="0 0 100 100">
+              <path
+                d="M100 100 L0 100 L0 0 A100 100 0 0 0 100 100 Z"
+                fill="#fe9a00"
+              />
+            </svg>
+          </div>
+        </>
+      )}
       All cards
     </button>
   );
--- app/components/tabs-container/tabs/DeckTab.jsx
+++ app/components/tabs-container/tabs/DeckTab.jsx
@@ -1,12 +1,18 @@
 import { useScreen } from "@/app/context/ScreenContext";
+import DeckScreen from "../../deck-screen/DeckScreen";
 
 export default function DeckNavButton() {
   const { currentScreen, setCurrentScreen } = useScreen();
 
   const styles = {
-    button: `h-full w-1/2 ${
+    button: `relative h-full w-1/2 ${
       currentScreen === "DeckScreen" ? "bg-amber-500" : ""
     }`,
+    // The translates are to prevent a rendering subpixel artifact
+    topCornerMask:
+      "absolute h-3/10 aspect-square top-0 right-[1px] transform: translateX(-1px) translate-x-full scale-x-[-1]",
+    bottomCornerMask:
+      "absolute h-1/6 aspect-square bottom-0 right-[-1px] transform: translateX(-1px) scale-x-[-1]",
   };
 
   return (
@@ -15,6 +21,26 @@ export default function DeckNavButton() {
       onMouseDown={() => setCurrentScreen("DeckScreen")}
       className={styles.button}
     >
+      {currentScreen === "DeckScreen" && (
+        <>
+          <div className={styles.topCornerMask}>
+            <svg className="w-full h-full" viewBox="0 0 100 100">
+              <path
+                d="M0 0 L100 0 L100 100 A100 100 0 0 0 0 0 Z"
+                fill="#fe9a00"
+              />
+            </svg>
+          </div>
+          <div className={styles.bottomCornerMask}>
+            <svg className="w-full h-full" viewBox="0 0 100 100">
+              <path
+                d="M100 100 L0 100 L0 0 A100 100 0 0 0 100 100 Z"
+                fill="#2b7fff"
+              />
+            </svg>
+          </div>
+        </>
+      )}
       Deck
     </button>
   );
