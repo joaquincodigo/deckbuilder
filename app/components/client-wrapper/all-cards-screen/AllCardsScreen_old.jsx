488dfd7cbe5ea3e416392f298ca44c6c02174c6f
joaquincodigo <joaquin.codigo@gmail.com>
2026-03-12 12:37:29 -0300

    Scroll position state is not lost anymore

--- app/components/client-wrapper/all-cards-screen/AllCardsScreen_old.jsx
+++ app/components/client-wrapper/all-cards-screen/AllCardsScreen_old.jsx
@@ -20,7 +20,7 @@ export default function AllCardsScreen({
     AllCardsScreen: `overflow-y-auto flex flex-col gap-y-2 bg-blue-900 text-white ${
       screen === "allCards"
         ? "opacity-100 visible h-full w-full"
-        : "opacity-0 invisible w-0 h-0"
+        : "opacity-0 invisible pointer-events-none absolute inset-0"
     }`,
   };
 
--- app/components/client-wrapper/deck-screen/DeckScreen.jsx
+++ app/components/client-wrapper/deck-screen/DeckScreen.jsx
@@ -1,14 +1,19 @@
 import DeckGrid from "./deck-grid/DeckGrid";
 import Header from "./header/Header";
+import { useEffect } from "react";
 
 export default function DeckScreen({ premadeDecks, deck, screen }) {
   const styles = {
     DeckScreen: `overflow-y-auto flex flex-col gap-y-2 ${
       screen === "deck"
         ? "opacity-100 visible h-full w-full"
-        : "opacity-0 invisible w-0 h-0"
+        : "opacity-0 invisible pointer-events-none absolute inset-0"
     }`,
   };
+
+  useEffect(() => {
+    console.log("deck state:", deck);
+  }, [deck]);
 
   return (
     <div data-component="DeckScreen" className={styles.DeckScreen}>
